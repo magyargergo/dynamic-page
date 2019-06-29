47718344da70f2c6eab240efb7603dbe26cca9e2
Gergő Magyar <gergomagyar@panyor.com>
2019-06-29 09:56:30 +0200

    Separate structure, style and behavior from each other

--- src/app/core/progress-bar/progress-bar.component.ts
+++ src/app/core/progress-bar/progress-bar.component.ts
@@ -1,34 +1,19 @@
-import { Component, Input } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-progress-bar',
-  template: `
-    <p-progressBar mode="{{ mode$ | async }}"></p-progressBar>
-  `,
-  styles: [`
-    :host ::ng-deep .ui-progressbar {
-        height: 4px;
-        background: #c5c5c5;
-    }`, `
-    :host ::ng-deep .ui-widget-header {
-        border: none;
-    }`, `
-    :host ::ng-deep .ui-widget-content {
-        border: none;
-    }`, `
-    :host ::ng-deep .ui-corner-all {
-        border-radius: 0;
-    }
-    `
-  ]
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
 })
 export class ProgressBarComponent {
   mode$ = this.router.events.pipe(
-    filter(evt => evt instanceof NavigationStart || evt instanceof NavigationEnd),
-    map(evt => evt instanceof NavigationStart ? 'indeterminate' : '')
+    filter(
+      evt => evt instanceof NavigationStart || evt instanceof NavigationEnd
+    ),
+    map(evt => (evt instanceof NavigationStart ? 'indeterminate' : ''))
   );
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 }
